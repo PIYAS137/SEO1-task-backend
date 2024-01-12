ef4f47f6569dbe9ee991f10dc5c7f3c3d3840dce
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2024-01-13 01:50:09 +0600

    files upload API is done

--- index.js
+++ index.js
@@ -13,51 +13,87 @@ app.use(cors());
 
 // ================================ MongoDB ======================================//
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.frg7rqf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
 
-    const taskCollection = client.db("taskDatabase").collection("taskCollection");
+        const taskCollection = client.db("taskDatabase").collection("taskCollection");
 
 
-    // ------------- Create New Task API -------------->>>>>>
-    app.post('/task',async(req,res)=>{
-        const data = req.body;
-        const result = await taskCollection.insertOne(data);
-        res.send(result);
-    })
-    
-    // ------------- Get All Task API -------------->>>>>>
-    app.get('/task',async(req,res)=>{
-        const result = await taskCollection.find({}).toArray();
-        res.send(result);
-    })
+        // ------------- Create New Task API -------------->>>>>>
+        app.post('/task', async (req, res) => {
+            const data = req.body;
+            const result = await taskCollection.insertOne(data);
+            res.send(result);
+        })
+
+        // ------------- Get All Task API -------------->>>>>>
+        app.get('/task', async (req, res) => {
+            const result = await taskCollection.find({}).toArray();
+            res.send(result);
+        })
+
+        // ------------- Update Task Status API -------------->>>>>>
+        app.patch('/task/:sid', async (req, res) => {
+            const status = req.body.status;
+            const id = req.params.sid;
+            const query = { _id: new ObjectId(id) };
+            const upatedDoc = {
+                $set: {
+                    status: status
+                }
+            }
+            const result = await taskCollection.updateOne(query, upatedDoc);
+            res.send(result)
+        })
+
+        // ------------- Delete Task Status API -------------->>>>>>
+        app.delete('/task/:sid',async(req,res)=>{
+            const id = req.params.sid;
+            const query = {_id : new ObjectId(id)};
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        })
+        
+        // ------------- Files Post API -------------->>>>>> 
+        app.post('/files/:sid',async(req,res)=>{
+            const id = req.params.sid;
+            const data = req.body;
+            const query = {_id : new ObjectId(id)};
+            const operation = {
+                $push : {
+                    files : data
+                }
+            }
+            const result = await taskCollection.updateOne(query,operation);
+            res.send(result)
+        })
 
 
 
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -68,11 +104,11 @@ run().catch(console.dir);
 
 
 
-app.get('/',async(req,res)=>{
+app.get('/', async (req, res) => {
     res.send("Server is running....!!!");
 })
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 })
